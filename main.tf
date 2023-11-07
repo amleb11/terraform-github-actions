e2dbd53de00b8821778943bc086e4e0eb2714b52
Allison LeBlanc <allison.leblanc@appdynamics.com>
2023-11-07 15:58:01 -0500

    Use our Terraform Cloud organization

--- main.tf
+++ main.tf
@@ -129,8 +129,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "TFResourceGroup"
-  #location = "eastus2"
+  name = "TFResourceGroup2"
   location = var.resource_group_location
   tags = {
     Environment = "UAT"
