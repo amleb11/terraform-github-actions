c3263da8b4db06457bcb5ae4ccb818775ff740b8
Allison LeBlanc <allison.leblanc@appdynamics.com>
2023-11-13 05:55:44 -0500

    Adding AWS EC2 App Server

--- main.tf
+++ main.tf
@@ -65,28 +65,6 @@ resource "aws_instance" "app_server" {
   }
 }
 
-
-#Create a web server
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-
-  tags = {
-    Name = "ACMEWebServerInstance9"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
 #Create an app server - Type and Name
 resource "aws_instance" "app_server" {
   ami           = "ami-01bc990364452ab3e"
